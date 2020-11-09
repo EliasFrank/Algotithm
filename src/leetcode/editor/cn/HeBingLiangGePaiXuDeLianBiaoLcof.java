6373e145ac1ac51d8fc9b3f181db50e92cab3d60
elias <elias_git@qq.com>
2020-11-09 20:46:18 +0800

    使用插件

---
+++ src/leetcode/editor/cn/HeBingLiangGePaiXuDeLianBiaoLcof.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn;
+
+public class HeBingLiangGePaiXuDeLianBiaoLcof{
+    public static void main(String[] args) {
+        Solution solution = new HeBingLiangGePaiXuDeLianBiaoLcof().new Solution();
+        
+    }
+public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+}
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
